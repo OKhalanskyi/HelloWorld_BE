1dc2bc3d12ed9d7794e1a26c5b8e42d2a04c9019
Khalanskyi Oleh <112695863+OKhalanskyi@users.noreply.github.com>
2023-10-10 03:34:35 +0300

    feat: cors enabled

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors();
+
   await app.listen(process.env.PORT, () => {
     console.log(`started on ${process.env.PORT}`)
   });
